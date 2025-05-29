9827f480c22ec07adc87a005c31e71629fba466c
ashraf <shaikhiramnaaz04@gmail.com>
2025-05-29 19:25:36 +0530

    Save before rebase

---
+++ String.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+   // char name[]={'I' , 'R', 'A', 'M', 'N', 'A', 'A', 'Z','\0'};
+    char name[]="Iramnaaz";
+
+    //char class[]={'C', 'O', 'D', 'E'};
+    char class[]="CODE";
+}
