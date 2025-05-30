c7a6e0d4956b43f0cc9b308f4c9a0a5ece412dd1
iramnaaz04 <shaikhiramnaaz04@gmail.com>
2025-05-30 12:01:36 +0530

    Structure lesson 1

---
+++ structure.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+struct student{
+    char name[100];
+    int roll;
+    float cgpa;
+};
+
+int main(){
+    struct student s1;
+    s1.roll = 1664;
+    s1.cgpa = 9.2;
+    strcpy(s1.name, "Iram");
+
+    printf("Student name: %s\n",s1.name);
+    printf("Student roll: %d\n",s1.roll);
+    printf("Student cgpa: %f\n",s1.cgpa);
+    return 0;
+}
