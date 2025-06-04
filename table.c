987df1c0728914d3b2fdfeb232389bccaeec6fd4
iramnaaz04 <shaikhiramnaaz04@gmail.com>
2025-06-04 21:02:49 +0530

    To print a table of n using while loop

---
+++ table.c
@@ -0,0 +1,12 @@
+// Table using while loop
+#include <stdio.h>
+void main(){
+    int i=1,n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    while (i <= 10){
+        printf("%d x %d = %d\n", n, i, n * i);
+        i++;
+    }
+}
