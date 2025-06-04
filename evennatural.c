66a33ce837d9c368c6798b56caeb13ca139486fd
iramnaaz04 <shaikhiramnaaz04@gmail.com>
2025-06-04 20:54:53 +0530

    Counts the number of even natural number upto n

---
+++ evennatural.c
@@ -0,0 +1,16 @@
+// Even natural from 1 to N
+#include <stdio.h>
+
+void main() {
+    int n, i = 1;
+
+    printf("Enter the value of N: ");
+    scanf("%d", &n);
+
+    while(i <= n) {
+        if(i % 2 == 0) {
+            printf("%d ", i);
+        }
+        i++;
+    }
+}
