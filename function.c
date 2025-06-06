1fe5fbe5e8e07b81548530fb0f007aab8b357366
iramnaaz04 <shaikhiramnaaz04@gmail.com>
2025-06-06 19:34:39 +0530

    Function lesson 1

---
+++ function.c
@@ -0,0 +1,10 @@
+// Function 
+#include <stdio.h>
+void greet(){
+    printf("Hello World!\n");
+}
+void main(){
+    greet();
+    printf("This is a function in C.\n");
+    printf("This is a function in C11.\n");
+}
