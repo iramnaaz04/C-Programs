03a1f51f96f11968d4f64818b7c40a2edb5a6990
iramnaaz04 <shaikhiramnaaz04@gmail.com>
2025-06-12 13:08:40 +0530

    ASCII values from A to Z using for loop

---
+++ ASCIIfor.c
@@ -0,0 +1,10 @@
+// ASCII values from A to Z
+#include <stdio.h>
+int main() {
+    for (char c = 'A'; c <= 'Z'; c++) {
+        printf("Character: %c, ASCII: %d\n", c, c);
+    }
+    return 0;
+}
+
+
