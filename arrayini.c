fb390c3750f22e841ef14983d010fa43ae793420
iramnaaz04 <shaikhiramnaaz04@gmail.com>
2025-06-26 13:03:48 +0530

    Initializing an array

---
+++ arrayini.c
@@ -0,0 +1,10 @@
+// Initialize an array
+#include <stdio.h>
+void main(){
+    int arr[5] = {1,2,3,4,5};
+    int i;
+    printf("Array elements: ");
+    for(i=0;i<5;i++){
+        printf("%d ",arr[i]);
+    }
+}
